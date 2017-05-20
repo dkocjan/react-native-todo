a2c49135cbd6bfc25a1438d93f5ee07715402f0c
Daniel Kocjan <dkocjan123@gmail.com>
2017-05-20 12:32:06 +0200

    Create text input

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,9 @@ class Header extends Component {
     return (
       <View style={styles.header}>
         <TextInput
+          value={this.props.value}
+          onChangeText={this.props.onChange}
+          onSubmitEditing={this.props.onAddItem}
           placeholder='What do you need to do?'
           blurOnSubmit={false}
           returnKeyType='done'
